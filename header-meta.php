c9ebb5be95c2559d7d69149396cf1969368524d3
Emily Davidson <emdavids@gmail.com>
2018-11-21 18:43:07 -0500

    Add brand font and new font-weight

--- header-meta.php
+++ header-meta.php
@@ -2,6 +2,7 @@
 <meta name="viewport" content="width=device-width">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link rel="stylesheet" href="https://use.typekit.net/cyu4heq.css">
 <!--[if lt IE 9]>
 <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
 <![endif]-->
